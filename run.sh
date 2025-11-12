0cc89ca4092de1e15a3fff4ee3ee034da7a4322a
Peter Meiring <pmeiring@pro.local>
2025-11-12 15:08:21 -0500

    cleanup

--- run.sh
+++ run.sh
@@ -2,6 +2,15 @@
 set -e
 unset PERL5LIB
 
+# Datacard folder (and rew scenario)
+eos=/eos/cms/store/group/phys_susy/SOS/limits_TChiWZ_neg_static_v2_merged/
+rew=neg
+
+# Output directory for toys and significances. Should contain subdirectories "toys" and "signif"
+toy=/eos/cms/store/group/phys_susy/SOS/someoutputdir/
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 # Parse inputs
 NTOYS=$1; shift
 MP=$1; shift
@@ -27,51 +36,46 @@ scram b -j 8
 cmsenv
 cd ${TMP}
 
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-eos=/eos/cms/store/group/phys_susy/SOS/limits_TChiWZ_neg_static_v2_merged/
-toy=/eos/cms/store/group/phys_susy/SOS/globalsignif_toys_negstaticv2/
-
 get_card() {
-	echo "Getting datacard and workspace from EOS"
-	masspoint="$1"
-	xrdcp root://eoscms.cern.ch/${eos}/cards/TChiWZ_Disp-neg_${masspoint}/card_TChiWZ_Disp-neg_${masspoint}_allEEMM.root ${TMP}/
-	xrdcp root://eoscms.cern.ch/${eos}/cards/TChiWZ_Disp-neg_${masspoint}/card_TChiWZ_Disp-neg_${masspoint}_allEEMM.txt ${TMP}/
-	mv ${TMP}/card_TChiWZ_Disp-neg_${masspoint}_allEEMM.root ${TMP}/mycard_${masspoint}.root
-	mv ${TMP}/card_TChiWZ_Disp-neg_${masspoint}_allEEMM.txt ${TMP}/mycard_${masspoint}.txt
+    echo "Getting datacard and workspace from EOS"
+    masspoint="$1"
+    xrdcp root://eoscms.cern.ch/${eos}/cards/TChiWZ_Disp-${rew}_${masspoint}/card_TChiWZ_Disp-${rew}_${masspoint}_allEEMM.root ${TMP}/
+    xrdcp root://eoscms.cern.ch/${eos}/cards/TChiWZ_Disp-${rew}_${masspoint}/card_TChiWZ_Disp-${rew}_${masspoint}_allEEMM.txt ${TMP}/
+    mv ${TMP}/card_TChiWZ_Disp-${rew}_${masspoint}_allEEMM.root ${TMP}/mycard_${masspoint}.root
+    mv ${TMP}/card_TChiWZ_Disp-${rew}_${masspoint}_allEEMM.txt ${TMP}/mycard_${masspoint}.txt
 }
 
 get_toys() {
-	echo "Getting toys file from EOS"
-	xrdcp root://eoscms.cern.ch/${toy}/toys_v2/higgsCombineTest.GenerateOnly.mH120.${SEED}.root ${TMP}/
+    echo "Getting toys file from EOS"
+    xrdcp root://eoscms.cern.ch/${toy}/toys/higgsCombineTest.GenerateOnly.mH120.${SEED}.root ${TMP}/
 }
 
 save_signif(){
-	echo "Sending significance file to EOS"
-	masspoint="$1"
-	xrdcp ${TMP}/higgsCombineTest.Significance.mH120.${SEED}.root root://eoscms.cern.ch/${toy}/signif/higgsCombineTest.Significance.${masspoint}.${SEED}.root
+    echo "Sending significance file to EOS"
+    masspoint="$1"
+    xrdcp ${TMP}/higgsCombineTest.Significance.mH120.${SEED}.root root://eoscms.cern.ch/${toy}/signif/higgsCombineTest.Significance.${masspoint}.${SEED}.root
 }
 
 save_toys(){
-	echo "Sending toys file to EOS"
-	xrdcp ${TMP}/higgsCombineTest.GenerateOnly.mH120.${SEED}.root root://eoscms.cern.ch/${toy}/toys_v2/
+    echo "Sending toys file to EOS"
+    xrdcp ${TMP}/higgsCombineTest.GenerateOnly.mH120.${SEED}.root root://eoscms.cern.ch/${toy}/toys/
 }
 
 # The b-only toys are always generated from the same card
 if [ "$GENERATE" == 1 ]; then
-	echo "Generating toys"
-	get_card 175_165_0
-	combine -M GenerateOnly -d ${TMP}/mycard_175_165_0.root -t ${NTOYS} --toysFreq --saveToys --seed ${SEED}
-	save_toys
+    echo "Generating toys"
+    get_card 175_165_0
+    combine -M GenerateOnly -d ${TMP}/mycard_175_165_0.root -t ${NTOYS} --toysFreq --saveToys --seed ${SEED}
+    save_toys
 fi
 
 # Use the b-only toys as input to compute local significance for MP
 if [ "$SIGNIFICANCE" == 1 ]; then
-	echo "Computing significance"
-	get_card ${MP}
-	get_toys
-	combine -M Significance -d ${TMP}/mycard_${MP}.root -t ${NTOYS} --toysFreq --toysFile=${TMP}/higgsCombineTest.GenerateOnly.mH120.${SEED}.root --seed ${SEED}
-	save_signif ${MP}
+    echo "Computing significance"
+    get_card ${MP}
+    get_toys
+    combine -M Significance -d ${TMP}/mycard_${MP}.root -t ${NTOYS} --toysFreq --toysFile=${TMP}/higgsCombineTest.GenerateOnly.mH120.${SEED}.root --seed ${SEED}
+    save_signif ${MP}
 fi
 
 cd ${TMP}/..
